7828005f46918f25dc2724a2f8ac0f2308b4d7c3
XSAM <xbs2468@gmail.com>
2016-03-14 12:05:05 +0800

    Use List and delay code let the snake-body follow the snake-head.Remove Coroutine funcion

---
+++ Assets/Scripts/SnakeBodyMovement.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SnakeBodyMovement : MonoBehaviour 
+{
+    private new Rigidbody rigidbody;
+    private SnakeHeadMovement snakeHeadMovement;
+    private float lastTime;
+    private bool isCoroutine;
+    private bool startTurn;
+    public float delayTime = 0.2f;
+
+    //private Quaternion lastTurn;
+    private Quaternion turn;
+    public List<Quaternion> lastTurn;
+
+	
+	// Use this for initialization
+	void Start () 
+	{
+        rigidbody = GetComponent<Rigidbody>();
+        snakeHeadMovement = GameObject.FindGameObjectWithTag("Head").GetComponent<SnakeHeadMovement>();
+        turn = Quaternion.identity;
+        isCoroutine = false;
+        startTurn = false;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+        if(!isCoroutine)
+        {
+            lastTime = Time.time;
+            isCoroutine = true;
+        }
+        if(lastTime+delayTime<=Time.time)
+        {
+            startTurn = true;
+        }
+        lastTurn.Add(snakeHeadMovement.lastTurn);
+        if(startTurn)
+        {
+            turn = lastTurn[0];
+            lastTurn.RemoveAt(0);
+        }
+
+		if(snakeHeadMovement.lastVelocity!=null)
+        {
+            rigidbody.velocity = snakeHeadMovement.lastVelocity.magnitude*transform.up;
+        }
+        if (turn != Quaternion.identity)
+        {
+            rigidbody.rotation = turn;
+        }
+
+	}
+}
--- Assets/Scripts/SnakeHeadMovement.cs
+++ Assets/Scripts/SnakeHeadMovement.cs
@@ -9,21 +9,32 @@ public class SnakeHeadMovement : MonoBehaviour
     private new Rigidbody rigidbody;
     private float horizontal;
     private float vertical;
+
+    [HideInInspector]
+    public Vector3 lastVelocity;
+    [HideInInspector]
+    public Quaternion lastTurn;
 	
 	// Use this for initialization
 	void Start () 
 	{
         rigidbody = GetComponent<Rigidbody>();
-
 	}
+
+    void Record()
+    { 
+        lastVelocity = rigidbody.velocity;
+        lastTurn = rigidbody.rotation;
+    }
 
     void FixedUpdate()
     {
         Vector3 movement = transform.up * vertical * speed;//*Time.deltaTime;
-        Debug.Log("movement:"+movement);
+        //Debug.Log("movement:"+movement);
         //rigidbody.MovePosition(movement+rigidbody.position);
         rigidbody.velocity = movement * Time.deltaTime;
         rigidbody.MoveRotation((Quaternion.Euler(new Vector3(0, horizontal * turnSpeed, 0) * Time.deltaTime) * rigidbody.rotation));
+        Record();
     }
 	
 	// Update is called once per frame
